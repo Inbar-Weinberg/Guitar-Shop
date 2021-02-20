31bbfe78956d8b0cf6c96c2f9d722398a490d3ea
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-20 23:38:32 +0200

    finished getters for classical Guitar

--- Guitar-Shop.js
+++ Guitar-Shop.js
@@ -13,13 +13,35 @@ function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price 
     this.manufactureYear = (isInRange(manufactureYear, 1900, DEF_MAN_YEAR)) ? manufactureYear : DEF_MAN_YEAR;
     this.brand = brand;
     this.price = (isInRange(price, 0)) ? price : DEF_PRICE;
-    this.numOfString = (isInRange(numOfString,1,20))? numOfString : DEF_STRINGS;
-    this.used=used;
-}
+    this.numOfString = (isInRange(numOfString, 1, 20)) ? numOfString : DEF_STRINGS;
+    this.used = used;
 
-function isInRange(num, min = 0, max = Infinity) {
-    num = parseFloat(num);
-    if (Number.isFinite && num > min && num < max)
-        return num;
-    return false;
-}🎶
+    ClassicGuitar.prototype.play = function () {
+        this.price *= 90 / 100;
+        this.used = true;
+        return "🎶🎶🎶";
+    }
+    ClassicGuitar.prototype.getManufactureYear = function () {
+        return this.manufactureYear;
+    }
+    
+    ClassicGuitar.prototype.getBrand = () => this.brand;
+    ClassicGuitar.prototype.getPrice = () => this.price;
+    ClassicGuitar.prototype.setPrice = (newPrice) => { this.price = newPrice };
+
+
+    function isInRange(num, min = 0, max = Infinity) {
+        num = parseFloat(num);
+        if (Number.isFinite && num > min && num < max)
+            return num;
+        return false;
+    }
+
+}
+// לסיים שיטות סטטיות
+// רלוונטי רק לclass 
+
+
+
+
+
