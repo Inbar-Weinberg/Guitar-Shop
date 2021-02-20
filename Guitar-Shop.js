a08acfaba3d4518e871f61ffc5e1719dbfa4166d
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-21 00:23:30 +0200

    added inheritance form Classic to Electrical Guitar

--- Guitar-Shop.js
+++ Guitar-Shop.js
@@ -4,17 +4,19 @@ const DEF_MAN_YEAR = 2021;
 const DEF_BRAND = 'unbranded';
 const DEF_PRICE = 1;
 const DEF_STRINGS = 6;
+const DEF_STRINGS_BASS = 4;
 const DEF_USED = false;
+const DEF_LONG_NECK = false;
 
 
 
-
-function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_PRICE, used = DEF_USED) {
+function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS, used = DEF_USED) {
     this.manufactureYear = (isInRange(manufactureYear, 1900, DEF_MAN_YEAR)) ? manufactureYear : DEF_MAN_YEAR;
-    this.brand = brand;
+    this.brand = (typeof brand === 'string') ? brand : DEF_BRAND;;
     this.price = (isInRange(price, 0)) ? price : DEF_PRICE;
     this.numOfString = (isInRange(numOfString, 1, 20)) ? numOfString : DEF_STRINGS;
-    this.used = used;
+    this.used = (typeof used === 'boolean') ? used : DEF_USED;
+
 
     ClassicGuitar.prototype.play = function () {
         this.price *= 90 / 100;
@@ -24,7 +26,7 @@ function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price 
     ClassicGuitar.prototype.getManufactureYear = function () {
         return this.manufactureYear;
     }
-    
+
     ClassicGuitar.prototype.getBrand = () => this.brand;
     ClassicGuitar.prototype.getPrice = () => this.price;
     ClassicGuitar.prototype.setPrice = (newPrice) => { this.price = newPrice };
@@ -39,8 +41,24 @@ function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price 
 
 }
 // לסיים שיטות סטטיות
-    
+
 // רלוונטי רק לclass 
+
+this.numOfString = (isInRange(numOfString, 1, 20)) ? numOfString : DEF_STRINGS;
+this.used = used;
+function ElectricGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS_BASS, used = DEF_USED, longNeck = DEF_LONG_NECK,) {
+
+    ClassicGuitar.call(this, manufactureYear, brand, price, numOfString, used);
+    this.longNeck = (typeof longNeck === 'boolean') ? longNeck : DEF_LONG_NECK;
+
+    ElectricGuitar.prototype= Object.create(ClassicGuitar.prototype);
+    Object.defineProperty(ElectricGuitar.prototype, 'constructor', {
+        value: ElectricGuitar,
+        enumerable: false, // so that it does not appear in 'for in' loop
+        writable: true
+    });
+
+}
 
 
 
