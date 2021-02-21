32111b09042457d9a8ded518d1c0c0df25ea6b95
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-21 02:54:07 +0200

    added static methods
    played around with all the different ways to write methods to classes

--- Guitar-Shop.js
+++ Guitar-Shop.js
@@ -8,6 +8,12 @@ const DEF_STRINGS_BASS = 4;
 const DEF_USED = false;
 const DEF_LONG_NECK = false;
 
+function isInRange(num, min = 0, max = Infinity) {
+    num = parseFloat(num);
+    if (Number.isFinite && num > min && num < max)
+        return num;
+    return false;
+}
 
 
 function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS, used = DEF_USED) {
@@ -17,50 +23,46 @@ function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price 
     this.numOfString = (isInRange(numOfString, 1, 20)) ? numOfString : DEF_STRINGS;
     this.used = (typeof used === 'boolean') ? used : DEF_USED;
 
-
-    ClassicGuitar.prototype.play = function () {
+    this.play = function () {
         this.price *= 90 / 100;
         this.used = true;
         return "🎶🎶🎶";
     }
-    ClassicGuitar.prototype.getManufactureYear = function () {
-        return this.manufactureYear;
-    }
-
-    ClassicGuitar.prototype.getBrand = () => this.brand;
-    ClassicGuitar.prototype.getPrice = () => this.price;
-    ClassicGuitar.prototype.setPrice = (newPrice) => { this.price = newPrice };
-
-
-    function isInRange(num, min = 0, max = Infinity) {
-        num = parseFloat(num);
-        if (Number.isFinite && num > min && num < max)
-            return num;
-        return false;
-    }
-
+    this.getManufactureYear = function () { return this.manufactureYear; }
+    this.getBrand = function () { return this.brand };
+    this.getPrice = () => this.price;
+    this.setPrice = (newPrice) => { this.price = newPrice };
 }
-// לסיים שיטות סטטיות
+ClassicGuitar.detectSound = function (sound) {
+    if (sound == "🎸") return 'ElectricGuitar';
+    if (sound === "🔊") return 'BassGuitar';
+    return 'sound not recognized'
+}
 
-// רלוונטי רק לclass 
 
-this.numOfString = (isInRange(numOfString, 1, 20)) ? numOfString : DEF_STRINGS;
-this.used = used;
-function ElectricGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS_BASS, used = DEF_USED, longNeck = DEF_LONG_NECK,) {
+
+function BassGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS_BASS, used = DEF_USED) {
 
     ClassicGuitar.call(this, manufactureYear, brand, price, numOfString, used);
-    this.longNeck = (typeof longNeck === 'boolean') ? longNeck : DEF_LONG_NECK;
 
-    ElectricGuitar.prototype= Object.create(ClassicGuitar.prototype);
-    Object.defineProperty(ElectricGuitar.prototype, 'constructor', {
-        value: ElectricGuitar,
+
+    BassGuitar.prototype = Object.create(ClassicGuitar.prototype);
+    Object.defineProperty(BassGuitar.prototype, 'constructor', {
+        value: BassGuitar,
         enumerable: false, // so that it does not appear in 'for in' loop
         writable: true
     });
 
+    this.play = function () {
+        this.price *= 90 / 100;
+        this.used = true;
+        return "🔊🔊🔊";
+    }
 }
 
+console.log(ClassicGuitar.detectSound("🔊"));
+//    this.longNeck = (typeof longNeck === 'boolean') ? longNeck : DEF_LONG_NECK;
+// longNeck = DEF_LONG_NECK
 
-
-
-
+let x = new BassGuitar();
+console.log(x.play());
