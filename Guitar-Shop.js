e536b3208b389e18a720e1569a28ce46f80817a6
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-21 03:15:51 +0200

    syntax fix

--- Guitar-Shop.js
+++ Guitar-Shop.js
@@ -1,5 +1,6 @@
 "use strict"
 // not using class syntax on purpose
+
 const DEF_MAN_YEAR = 2021;
 const DEF_BRAND = 'unbranded';
 const DEF_PRICE = 1;
@@ -8,12 +9,7 @@ const DEF_STRINGS_BASS = 4;
 const DEF_USED = false;
 const DEF_LONG_NECK = false;
 
-function isInRange(num, min = 0, max = Infinity) {
-    num = parseFloat(num);
-    if (Number.isFinite && num > min && num < max)
-        return num;
-    return false;
-}
+
 
 function ClassicGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS, used = DEF_USED) {
     this.manufactureYear = (isInRange(manufactureYear, 1900, DEF_MAN_YEAR)) ? manufactureYear : DEF_MAN_YEAR;
@@ -37,8 +33,6 @@ ClassicGuitar.detectSound = function (sound) {
     if (sound === "🔊") return 'BassGuitar';
     return 'sound not recognized'
 }
-
-
 
 function BassGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price = DEF_PRICE, numOfString = DEF_STRINGS_BASS, used = DEF_USED) {
     ClassicGuitar.call(this, manufactureYear, brand, price, numOfString, used);
@@ -81,4 +75,12 @@ function ElectricGuitar(manufactureYear = DEF_MAN_YEAR, brand = DEF_BRAND, price
         this.used = true;
         return "🎸🎸🎸";
     }
+}
+
+
+function isInRange(num, min = 0, max = Infinity) {
+    num = parseFloat(num);
+    if (Number.isFinite && num > min && num < max)
+        return num;
+    return false;
 }
